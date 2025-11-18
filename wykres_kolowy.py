0cf008f1a3968a638db70bf21cbea817361376d8
agakotala <agnieszka.szubryt09@gmail.com>
2025-11-18 21:38:29 +0100

    Update wykres_kolowy.py

--- wykres_kolowy.py
+++ wykres_kolowy.py
@@ -7,7 +7,13 @@ df = pd.read_csv("klienci.csv")
 city_counts = df['miasto'].value_counts()
 
 plt.figure(figsize = (10,10))
-plt.pie(city_counts, labels = city_counts.index, autopct='%1.1f%%', startangle=90)
+#rysujemy wykres kołowy na podstawie liczby klientów w miastach
+plt.pie(
+    city_counts,                   #wartości – liczba klientów w miastach
+    labels = city_counts.index,    #etykiety – nazwy miast
+    autopct='%1.1f%%',             #wyświetlanie procentów z dokładnością do jednego miejsca po przecinku
+    startangle=90                  #obracamy wykres, żeby pierwszy segment zaczynał się od godziny 12
+)
 plt.title('Udział klientów w poszczegolnych maistach')
 plt.tight_layout()
 plt.savefig("wykres_kolowy_miasta.png",dpi=300)
