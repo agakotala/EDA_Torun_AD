0e7a9910f14f30e0c6e4027061aafb46aef4b5db
agakotala <agnieszka.szubryt09@gmail.com>
2025-11-18 21:47:42 +0100

    Update funkcje.py

--- funkcje.py
+++ funkcje.py
@@ -1,10 +1,18 @@
+#definiujemy funkcję o nazwie kategorie_miast, która przyjmuje jeden argument: miasto
 def kategorie_miast(miasto):
+    #tworzymy listę dużych miast, które chcemy traktować jako "duże"
     Duze_Miasta = ["Warszawa", "Kraków", "Gdańsk", "Wrocław"]
-    if miasto in Duze_Miasta:
-        return "duże miasto"
-    elif len(miasto) > 8:
-        return "średnie miasto"
-    else:
-        return "małe miasto"
 
+    #sprawdzamy, czy podane miasto znajduje się na liście dużych miast
+    if miasto in Duze_Miasta:
+        return "duże miasto"         # eśli tak — zwracamy kategorię "duże miasto"
+
+    #jeśli nie jest dużym miastem, sprawdzamy, czy jego nazwa jest długa (więcej niż 8 znaków)
+    elif len(miasto) > 8:
+        return "średnie miasto"      #jeśli nazwa jest dłuższa — traktujemy je jako "średnie"
+
+    else:
+        return "małe miasto"         #jeżeli nie spełnia żadnego z powyższych warunków — jest "małe"
+
+#wywołujemy funkcję z argumentem "Pcim" i drukujemy wynik
 print(kategorie_miast("Pcim"))
